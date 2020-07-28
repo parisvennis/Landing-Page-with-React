4f4cd0db0848caa723ddb4f4452152c232726186
parisvennis <parisvennis@gmail.com>
2020-07-28 19:17:18 +0000

    updates

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,18 +1,34 @@
 import React from "react";
 
 export function Card() {
-	const cardTitles = ["card1", "card2", "card3", "card4"];
+	const cardTitles = [
+		{
+			title: "card1",
+			imgUrl: "https://www.wv.gov/images/homebkg.jpg"
+		},
+		{
+			title: "card2",
+			imgUrl:
+				"https://www.google.com/search?q=puppy&tbm=isch&ved=2ahUKEwjUnOGTtvDqAhWuazABHcuoAHEQ2-cCegQIABAA&oq=puppy&gs_lcp=CgNpbWcQAzIECCMQJzIECAAQQzIHCAAQsQMQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIFCAAQsQMyBQgAELEDMgUIABCxAzoCCABQ2ChY3DJg_jRoAHAAeACAAWCIAakDkgEBNZgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=0FkgX9T0KK7XwbkPy9GCiAc&bih=723&biw=708&rlz=1C5CHFA_enUS909US909#imgrc=UCqmZQ78EtHrYM"
+		},
+		{
+			title: "card3",
+			imgUrl:
+				"https://www.google.com/search?q=puppy&tbm=isch&ved=2ahUKEwjUnOGTtvDqAhWuazABHcuoAHEQ2-cCegQIABAA&oq=puppy&gs_lcp=CgNpbWcQAzIECCMQJzIECAAQQzIHCAAQsQMQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIFCAAQsQMyBQgAELEDMgUIABCxAzoCCABQ2ChY3DJg_jRoAHAAeACAAWCIAakDkgEBNZgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=0FkgX9T0KK7XwbkPy9GCiAc&bih=723&biw=708&rlz=1C5CHFA_enUS909US909#imgrc=UCqmZQ78EtHrYM"
+		},
+		{
+			title: "card4",
+			imgUrl:
+				"https://www.google.com/search?q=puppy&tbm=isch&ved=2ahUKEwjUnOGTtvDqAhWuazABHcuoAHEQ2-cCegQIABAA&oq=puppy&gs_lcp=CgNpbWcQAzIECCMQJzIECAAQQzIHCAAQsQMQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIFCAAQsQMyBQgAELEDMgUIABCxAzoCCABQ2ChY3DJg_jRoAHAAeACAAWCIAakDkgEBNZgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=0FkgX9T0KK7XwbkPy9GCiAc&bih=723&biw=708&rlz=1C5CHFA_enUS909US909#imgrc=UCqmZQ78EtHrYM"
+		}
+	];
 	const cardList = cardTitles.map(function(hand, i) {
 		return (
 			<div className="card m-1" key={i} style={{ width: "18rem" }}>
-				<img src="..." className="card-img-top" alt=".." />
+				<img src={hand.imgUrl} className="card-img-top" alt=".." />
 				<div className="card-body">
-					<h5 className="card-title">Card title</h5>
-					<p className="card-text">
-						{
-							"Some quick example text to build on the card title and make up the bulk of the card's content."
-						}
-					</p>
+					<h5 className="card-title">{hand.title}</h5>
+					<p className="card-text">{hand.message}</p>
 					<a href="#" className="btn btn-primary">
 						Go somewhere
 					</a>
